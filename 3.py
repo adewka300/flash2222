96570605a171798fb8c867a9526ef97138e7c86e
adewka300 <109631649+adewka300@users.noreply.github.com>
2022-07-22 12:48:03 +0300

    Update 3.py

--- 3.py
+++ 3.py
@@ -1,15 +1,14 @@
-# №3
 m1 = [''] + [oct(x)[2:] for x in range(0, 200)]
-m2 = ['0' + str(x * 2) for x in range(0, 100)] + \
-     [str(x * 2) for x in range(0, 1000)]
+m2 = ['0' + str(x) for x in range(0, 100)] + \
+     [str(x) for x in range(0, 1000)]
 mas = []
 
 for i in m1:
     for j in m2:
         m = '123#4%5'.replace('#', i).replace('%', j)
-        if int(m) < 10 ** 8 and int(m) % 221 == 0:
+        if int(m) < 10 ** 8 and int(m) % 333 == 0:
             mas.append(int(m))
 
 mas = sorted(mas)
 for a in mas:
-    print(a, a // 221)
+    print(a, a // 333)
